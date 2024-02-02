211f793c69916373e00b0eb7539bacd72283abc2
David Mondok <3883758+davidmondok@users.noreply.github.com>
2024-02-02 14:14:34 +0100

    improve logger style, add colorizer class

---
+++ src/Colorizer.php
@@ -0,0 +1,153 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaiseki\WordPress\WpCli\Util;
+
+class Colorizer
+{
+    protected function colorize(string $message, string $color): string
+    {
+        return \WP_CLI::colorize("%{$color}{$message}%n");
+    }
+
+    public function toYellow(string $message): string
+    {
+        return $this->colorize($message, 'y');
+    }
+
+    public function toGreen(string $message): string
+    {
+        return $this->colorize($message, 'g');
+    }
+
+    public function toBlue(string $message): string
+    {
+        return $this->colorize($message, 'b');
+    }
+
+    public function toRed(string $message): string
+    {
+        return $this->colorize($message, 'r');
+    }
+
+    public function toMagenta(string $message): string
+    {
+        return $this->colorize($message, 'm');
+    }
+
+    public function toCyan(string $message): string
+    {
+        return $this->colorize($message, 'c');
+    }
+
+    public function toGrey(string $message): string
+    {
+        return $this->colorize($message, 'w');
+    }
+
+    public function toBlack(string $message): string
+    {
+        return $this->colorize($message, 'k');
+    }
+
+    public function toYellowBright(string $message): string
+    {
+        return $this->colorize($message, 'Y');
+    }
+
+    public function toGreenBright(string $message): string
+    {
+        return $this->colorize($message, 'G');
+    }
+
+    public function toBlueBright(string $message): string
+    {
+        return $this->colorize($message, 'B');
+    }
+
+    public function toRedBright(string $message): string
+    {
+        return $this->colorize($message, 'R');
+    }
+
+    public function toMagentaBright(string $message): string
+    {
+        return $this->colorize($message, 'M');
+    }
+
+    public function toCyanBright(string $message): string
+    {
+        return $this->colorize($message, 'C');
+    }
+
+    public function toGreyBright(string $message): string
+    {
+        return $this->colorize($message, 'W');
+    }
+
+    public function toBlackBright(string $message): string
+    {
+        return $this->colorize($message, 'K');
+    }
+
+    public function toBackgroundYellow(string $message): string
+    {
+        return $this->colorize($message, '3');
+    }
+
+    public function toBackgroundGreen(string $message): string
+    {
+        return $this->colorize($message, '2');
+    }
+
+    public function toBackgroundBlue(string $message): string
+    {
+        return $this->colorize($message, '4');
+    }
+
+    public function toBackgroundRed(string $message): string
+    {
+        return $this->colorize($message, '1');
+    }
+
+    public function toBackgroundMagenta(string $message): string
+    {
+        return $this->colorize($message, '5');
+    }
+
+    public function toBackgroundCyan(string $message): string
+    {
+        return $this->colorize($message, '6');
+    }
+
+    public function toBackgroundGrey(string $message): string
+    {
+        return $this->colorize($message, '7');
+    }
+
+    public function toBackgroundBlack(string $message): string
+    {
+        return $this->colorize($message, '0');
+    }
+
+    public function toStyleBlink(string $message): string
+    {
+        return $this->colorize($message, 'F');
+    }
+
+    public function toStyleUnderline(string $message): string
+    {
+        return $this->colorize($message, 'U');
+    }
+
+    public function toStyleInverse(string $message): string
+    {
+        return $this->colorize($message, '8');
+    }
+
+    public function toStyleBright(string $message): string
+    {
+        return $this->colorize($message, '9');
+    }
+}
--- src/Logger.php
+++ src/Logger.php
@@ -6,18 +6,38 @@ namespace Kaiseki\WordPress\WpCli\Util;
 
 class Logger
 {
+    public function __construct(private readonly Colorizer $colorizer)
+    {
+    }
+
     public function log(string $message): void
     {
         \WP_CLI::log($message);
     }
 
-    public function logSuccess(string $message): void
+    public function success(string $message, bool $indent = false): void
     {
-        \WP_CLI::success("✅ " . $message);
+        $indentString = $this->getIndentString($indent);
+        $prefix = $this->colorizer->toGreen("Success:");
+        \WP_CLI::log("{$indentString}✅ {$prefix} {$message}");
     }
 
-    public function logError(string $message): void
+    public function warning(string $message, bool $indent = false): void
     {
-        \WP_CLI::error("❌ " . $message);
+        $indentString = $this->getIndentString($indent);
+        $prefix = $this->colorizer->toYellow("Warning:");
+        \WP_CLI::log("{$indentString}❌ {$prefix} {$message}");
+    }
+
+    public function error(string $message, bool $indent = false): void
+    {
+        $indentString = $this->getIndentString($indent);
+        $prefix = $this->colorizer->toRed("Error:");
+        \WP_CLI::log("{$indentString}⚠️ {$prefix} {$message}");
+    }
+
+    private function getIndentString(bool $indent): string
+    {
+        return $indent ? '↳   ' : '';
     }
 }
